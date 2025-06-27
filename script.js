ad6516de8b81351a01550bd625636464d8e247e7
devathrv <shivanshshukla908@gmail.com>
2025-06-27 10:14:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
+const hamburger = document.getElementById('hamburger');
+const overlayMenu = document.getElementById('overlay-menu');
+const closeBtn = document.getElementById('close');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  menu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  overlayMenu.style.left = '0';
+  hamburger.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', () => {
+  overlayMenu.style.left = '-100%';
+  hamburger.style.display = 'block';
 });
